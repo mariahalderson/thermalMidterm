8059e4ff76cdb302ff36720f787b820ab77eb8d0
Mariah Alderson <mariah.alderson@gmail.com>
2018-10-10 12:11:21 -0400

    hamburger menu functions finished

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 //hamburger menu
 var hamburger = document.querySelector("#hamburger"),
-    hamburgerLines = Array.from(document.querySelectorAll(".hamburger-line"));
+    hamburgerLines = Array.from(document.querySelectorAll(".hamburger-line")),
+    navigation = document.querySelector("#mainNavigation");
 
 
     hamburger.addEventListener('click', function(){
         hamburgerLines.forEach(line=>line.classList.toggle("change"));
+        navigation.classList.toggle("change");
     });
